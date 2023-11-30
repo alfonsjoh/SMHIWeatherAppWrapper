f121bb9539c2bbd985e4778c2d80dafca41a41bb
ealfjoh002 <ealfjoh002@utb.hoglandet.se>
2023-11-30 14:18:08 +0100

    Search field & Scrollbar

---
+++ App/WeatherApp/Views/Shared/Components/Navbar.cshtml.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WeatherApp.Views.Shared.Components;
+
+public class Navbar : PageModel
+{
+    public void OnGet()
+    {
+        
+    }
+}
