43c4f42f62d8fde7b89296143736b99af8cfc16c
alfonjoh <alfonsjoh@live.com>
2024-02-14 13:23:27 +0100

    Logic based weather description generator

---
+++ App/WeatherApp/Models/WeatherTipGeneration/LogicBased/ForecastDescription.cs
@@ -0,0 +1,21 @@
+﻿using System.Text.Json.Serialization;
+using WeatherApp.Models.Toolbox;
+using WeatherApp.Models.Weather.Models;
+
+namespace WeatherApp.Models.WeatherTipGeneration.LogicBased;
+
+public class ForecastDescriptor
+{
+	private readonly Random _rng = new();
+	[JsonPropertyName("keyword")]
+	public string Keyword { get; set; }
+	[JsonPropertyName("descriptions")]
+	public string[] Descriptions { get; set; }
+	
+	public bool IsApplicable(string mostCommonDescription) => mostCommonDescription.Contains(Keyword);
+
+	public IEnumerable<string> GetForecastDescriptions()
+	{
+		yield return Descriptions.RandomElement(_rng);
+	}
+}
---
+++ App/WeatherApp/Models/WeatherTipGeneration/LogicBased/Generator.cs
@@ -0,0 +1,38 @@
+﻿using System.Text.Json;
+using WeatherApp.Models.Toolbox;
+using WeatherApp.Models.Weather.Models;
+
+namespace WeatherApp.Models.WeatherTipGeneration.LogicBased;
+
+public class LogicBasedWeatherDescriptionGenerator : IWeatherDescriptionGenerator
+{
+	private const string ForecastDescriptorAddersFile = "./ForecastDescriptions.json";
+
+	private readonly ForecastDescriptor[] _forecastDescriptorAdders;
+	
+	public LogicBasedWeatherDescriptionGenerator()
+	{
+		// Read Forecast descriptor adders from json file
+		using (var sr = new StreamReader(ForecastDescriptorAddersFile))
+		{
+			var json = sr.ReadToEnd();
+			var forecastDescriptorAdders = JsonSerializer.Deserialize<ForecastDescriptor[]>(json);
+			if (forecastDescriptorAdders == null)
+			{
+				throw new Exception("Failed to deserialize ForecastDescriptorAdders");
+			}
+			
+			_forecastDescriptorAdders = forecastDescriptorAdders;
+		}
+	}
+    
+	public void SetWeatherDescription(ref ForecastModel forecast)
+	{
+		var model = forecast;
+		var mostCommonDescription = model.Prognosis.Select(weather => weather.Icon.Description).MostCommon().ToLower();
+		
+		forecast.Description = string.Join(" ", _forecastDescriptorAdders
+			.Where(descriptorAdder => descriptorAdder.IsApplicable(mostCommonDescription))
+			.SelectMany(descriptorAdder => descriptorAdder.GetForecastDescriptions()).ToArray());
+	}
+}
